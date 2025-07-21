0c4ef456e56b46169bb440aaf045d5a6b2cd8e67
sarveshwarwar <sarveshwarwar@gmail.com>
2025-07-21 06:43:04 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -1,22 +1,27 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 import pickle
 
-# Sample dummy data
-df = pd.DataFrame({
-    'temperature': [30, 22, 35, 18, 40],
-    'humidity': [40, 60, 20, 90, 15],
-    'pressure': [1012, 1015, 1010, 1008, 1020],
-    'wind_speed': [10, 5, 20, 12, 7],
-    'rain': [0, 1, 0, 1, 0]  # Target variable
-})
+# Sample weather dataset
+data = {
+    'temperature': [30, 22, 25, 28, 35, 18, 15],
+    'humidity': [80, 65, 70, 75, 90, 60, 55],
+    'wind_speed': [12, 10, 9, 11, 13, 7, 6],
+    'pressure': [1010, 1005, 1008, 1012, 1002, 1007, 1006],
+    'rain': [1, 0, 0, 1, 1, 0, 0]
+}
 
-X = df[['temperature', 'humidity', 'pressure', 'wind_speed']]
+df = pd.DataFrame(data)
+
+X = df.drop('rain', axis=1)
 y = df['rain']
 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
 model = LogisticRegression()
-model.fit(X, y)
+model.fit(X_train, y_train)
 
 # Save the model
-with open('model.pkl', 'wb') as f:
+with open('weather_model.pkl', 'wb') as f:
     pickle.dump(model, f)
